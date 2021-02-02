2a0068b767e674f6b39014cbe3be19eaaeff79f7
Glitch (pk-pal) <none>
2021-02-02 18:17:19 +0000

    👗⛩ Updated with Glitch

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ client = commands.Bot(command_prefix = '\pkp ')
 @client.event
 async def on_ready():
   await client.change_presence(status=discord.Status.online, activity=discord.Game('Online & Ready!'))
+  print(f'{client.user} has connected to Discord!')
   logging.info(f'{client.user} has connected to Discord!')
     
     
